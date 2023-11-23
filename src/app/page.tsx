4a5e9a5f2574c8898e72de5cf7de7c605e4d11a4
vinay.katta <vinay.katta@servicenow.com>
2023-11-23 10:43:27 -0600

    Handle build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
 
-import menu from "@/data/menu";
-import Image from 'next/image';
-
-import { MenuList } from "@/components/menu/MenuList";
+import menu from "@/data/menu.js";
+import Image from 'next/image'
 
 export default function Home() {
+
   return (
     <>
-      <MenuList menu={menu} />
-      
+        <div className="content-section-title">Menu</div>
+      <div className="content-list">
+        { menu.map(item =>
+          <div className="content-item" key={item.id}>
+            <div className="content-item__image-container">
+              <Image 
+                src={item.image} 
+                style={{objectFit: "cover"}}
+                sizes="(max-width: 768px)"
+                fill={true}
+                alt={item.name} 
+              />
+            </div>
+            <div className="content-item__header">
+              <div>{item.name}</div>
+              <div>{item.description}</div>
+            </div>
+
+          </div>
+        )}
+      </div>
     </>
   )
 }
